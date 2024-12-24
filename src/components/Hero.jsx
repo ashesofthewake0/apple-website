19f8a800b485133d4ed36f09a2ce6ddd85b603ab
Sarp Bilgiç <124276905+ashesofthewake0@users.noreply.github.com>
2024-12-24 17:28:20 +0200

    VideoCarousel component finished

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
     window.addEventListener('resize', handleVideoSrcSet);
 
     return () => {
-      window.removeEventListener('reisze', handleVideoSrcSet)
+      window.removeEventListener('resize', handleVideoSrcSet)
     }
   }, [])
 
@@ -49,4 +49,4 @@ const Hero = () => {
   )
 }
 
-export default Hero
+export default Hero;
